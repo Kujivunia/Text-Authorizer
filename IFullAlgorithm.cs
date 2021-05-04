6aa19d9e0afac3ce401d99b455e71e0a22a8b83f
Kujivunia <Kujivunia@KUJIVUNIA-PC>
2021-05-04 20:15:28 +0600

    Добавьте файлы проекта.

---
+++ IFullAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAuthorizerNC
+{
+    interface IFullAlgorithm: IAlgorithmStep
+    {
+        void AddStep(IAlgorithmStep step);
+        void RemoveStepAt(int i);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TextAuthorizerNC
+{
+     class Test
+    {
+        private System.Collections.Generic.Queue<int> queue = new System.Collections.Generic.Queue<int>();
+        public void AddX(int x)
+        {
+            queue.Enqueue(x);
+        }
+        public void RemoveX()
+        {
+            queue.Dequeue();
+        }
+        public void ShowX()
+        {
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    class Program
+    {
+        static public void PrintList(int ii, System.Collections.Generic.List<int> list)
+        {
+            for (int i = 0; i < ii; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Collections.Generic.List<int> list = new System.Collections.Generic.List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            Program.PrintList(4, list);
+            Console.WriteLine("---------------------------------");
+            Test test = new Test();
+            test.AddX(4);
+            test.AddX(2);
+
+            test.ShowX();
+            test.RemoveX();
+            test.ShowX();
+            Console.ReadKey();
+        }
+    }
+}
